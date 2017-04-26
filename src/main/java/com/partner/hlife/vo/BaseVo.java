f6f6160b4b8cb3f77f4cb6b2e482d729176aa494
190665565 <190665565@qq.com>
2017-04-26 21:42:49 +0800

    Merge remote-tracking branch 'remotes/origin/master' into work
    # Conflicts:
    #	readme.txt

---
+++ src/main/java/com/partner/hlife/vo/BaseVo.java
@@ -0,0 +1,25 @@
+package com.partner.hlife.vo;
+
+/**
+ * Created by changjiwang on 2017/4/26.
+ */
+public class BaseVo {
+    private int area_id;
+    private String sign;
+
+    public int getArea_id() {
+        return area_id;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setArea_id(int area_id) {
+        this.area_id = area_id;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
---
+++ src/main/java/com/partner/hlife/vo/CommonQueryVO.java
@@ -0,0 +1,16 @@
+package com.partner.hlife.vo;
+
+/**
+ * Created by changjiwang on 2017/4/26.
+ */
+public class CommonQueryVO extends BaseVo{
+    private int m_id;
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+}
